081f84a215b20a227ec50bc9be35cc0fdb869d3d
Ahmed Mamdouh <ahmed.mamdouh@coformatique.com>
2023-02-04 20:45:32 +0200

    feat: update circleci config file

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -1,11 +1,12 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=myPassword
-export POSTGRES_HOST=mydbinstance.csxbuclmtj3c.us-east-1.rds.amazonaws.com
+export POSTGRES_USERNAME=mamdouh
+export POSTGRES_PASSWORD=Ahmed.321
+export POSTGRES_HOST=udagram-db.cym3wyagliht.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::myawsbucket-75139724085
+export AWS_BUCKET=arn:aws:s3:::udagram-web
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
+export API_URL=http://udagram-api-dev2.us-east-1.elasticbeanstalk.com/api/v0
 export JWT_SECRET=mysecretstring
 export URL=http://localhost:8100
